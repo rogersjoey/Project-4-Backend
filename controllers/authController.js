f1aed56eda2500fb43a7a0cfbd94178f1f655baa
Joseph Rogers <RogersJoseph@JohnDeere.com>
2020-12-14 14:31:50 -0600

    All Routes Are Complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,37 +15,35 @@ router.get("/logout", (req, res) => {
 // POST - CREATE NEW USER FROM SIGNUP
 router.post("/signup", (req, res) => {
   console.log(req.body);
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) return res.status(500).json(err);
-
-    bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
-      if (err) return res.status(500).json(err);
-      req.body.password = hashedPwd;
-
-      UserModel.create(req.body)
-        .then((newUser) => {
-          const token = jwt.sign(
-            {
-              username: newUser.username,
-              id: newUser.id,
-            },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: "30 days",
-            }
-          );
-          console.log(token);
-          // res.cookie("jwt", token); // SEND A NEW COOKIE TO THE BROWSER TO STORE TOKEN
-          // res.redirect(`/users/profile/${newUser.id}`);
-          res.json({ token, newUser });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.send(`err ${err}`);
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) return res.status(500).json(err);
+        bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+          if (err) return res.status(500).json(err);
+          req.body.password = hashedPwd;
+          UserModel.create(req.body)
+            .then((newUser) => {
+              const token = jwt.sign(
+                {
+                  username: newUser.username,
+                  id: newUser.id,
+                },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: "30 days",
+                }
+              );
+              console.log(token);
+              // res.cookie("jwt", token); // SEND A NEW COOKIE TO THE BROWSER TO STORE TOKEN
+              // res.redirect(`/users/profile/${newUser.id}`);
+              res.json({ token, newUser });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.send(`err ${err}`);
+            });
         });
-    });
-  });
-});
+      });
+bvn});
 
 // POST LOGIN
 router.post("/login", (req, res) => {
